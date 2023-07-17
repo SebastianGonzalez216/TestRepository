e2488c56568167b40006eb14396640ffb73504cb
SebasDev <psi.ragnar@gmail.com>
2023-07-17 16:39:23 -0600

    MI primer commit

---
+++ HolaMundo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MiHolaMundo
+{
+    public class HolaMundo
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hola mundo! estoy aprendiendo a usar GIT");
+        }
+    }
+}
